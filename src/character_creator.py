3e5e9c7e405df868a8ff4fba489079b838a26504
Annaliese <Annaliesepalm3@gmail.com>
2025-04-30 12:09:44 -0500

    adding different classes and skill types

--- src/character_creator.py
+++ src/character_creator.py
@@ -2,3 +2,18 @@ import random
 from enum import Enum
 import tkinter as tk
 from tkinter import ttk, messagebox
+
+class TypeClass(Enum):
+    NONE = "No Magic(Villager)"
+    WIZARD = "Wizard"
+    SORCERER = "Sorcerer"
+    WARLOCK = "Warlock"
+    NECROMANCER = "Necromancer"
+    CLERIC = "Cleric"
+    PREIST =
+    MONK =
+    DRUID =
+    BARD =
+    RANGER =
+    PALADIN =
+    ARTIFICER =
